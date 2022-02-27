00552d1155681ed8f83d9bc116f87efd3c24dae1
bobby-webdev2021 <85532644+bobby-webdev2021@users.noreply.github.com>
2022-02-26 19:06:49 -0600

    Add files via upload

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,14 +1,34 @@
 // Declare any necessary variables.
 
-// Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
+// Add an evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
+document.querySelector("#print").addEventListener("click", () => printCert(newStudent()))
 
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
+document.querySelector("#reset").addEventListener("click", () => {
+    [...document.querySelectorAll("input")].forEach(e => e.value = "");
+    [...document.querySelectorAll(".cert-data")].forEach(e => e.textContent = "");
+})
 
 // Create a function that instantiates a new student object with the input from the HTML form.
+function newStudent() {
+    let stuName = document.querySelector("#studentName").value;
+    let courseName = document.querySelector("#className").value;
+    let stuScores = convertToNum(document.querySelector("#studentScores").value);
+    let possScores = convertToNum(document.querySelector("#possibleScores").value);
+    return new Student(stuName, courseName, stuScores, possScores);
+}
 
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
+function printCert(cs) {
+    document.querySelector("#certStudentName").textContent = cs.getName();
+    document.querySelector("#certClassName").textContent = cs.getCourse();
+    document.querySelector("#certGrade").textContent = cs.ltrGrade();
+}
 
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
+function convertToNum(arr) {
+    return arr.split(",").map(e => parseInt(e));
+}
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -3,3 +3,36 @@
 // - a method that adds up all the student's scores
 // - a method that adds up all the possible scores
 // - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
+class Student {
+    constructor(name, course, scores, maxScores) {
+        this.name = name;
+        this.course = course;
+        this.scores = scores;
+        this.maxScores = maxScores;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getCourse() {
+        return this.course;
+    }
+
+    sumScores() {
+        return this.scores.reduce((p, c) => (p + c));
+    }
+
+    possScores() {
+        return this.maxScores.reduce((p, c) => (p + c));
+    }
+
+    ltrGrade() {
+        let totalScore = (this.sumScores()/this.possScores()) * 100
+        if (totalScore >= 90) return "A";
+        if (totalScore >= 80) return "B";
+        if (totalScore >= 70) return "C";
+        if (totalScore >= 60) return "D";
+        else return "F";
+    }
+}
